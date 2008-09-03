648dff98814d2ddeda2f06a8c0e2fb01547fac2a
Marek Stępniowski <marek@stepniowski.com>
2008-09-03 18:24:30 +0200

    Moved book2html and bookfragments into its own librarian package.

--- lib/librarian/html.py
+++ lib/librarian/html.py
@@ -1,10 +1,59 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import optparse
 import os
+import cStringIO
+import re
 import copy
+import pkgutil
 
 from lxml import etree
+
+
+ENTITY_SUBSTITUTIONS = [
+    (u'---', u'—'),
+    (u'--', u'–'),
+    (u'...', u'…'),
+    (u',,', u'„'),
+    (u'"', u'”'),
+]
+
+
+def substitute_entities(context, text):
+    """XPath extension function converting all entites in passed text."""
+    if isinstance(text, list):
+        text = ''.join(text)
+    for entity, substitutution in ENTITY_SUBSTITUTIONS:
+        text = text.replace(entity, substitutution)
+    return text
+
+
+# Register substitute_entities function with lxml
+ns = etree.FunctionNamespace('http://wolnelektury.pl/functions')
+ns['substitute_entities'] = substitute_entities
+
+
+def transform(input_filename, output_filename):
+    """Transforms file input_filename in XML to output_filename in XHTML."""
+    # Parse XSLT
+    style_filename = os.path.join(os.path.dirname(__file__), 'book2html.xslt')
+    style = etree.parse(style_filename)
+
+    doc_file = cStringIO.StringIO()
+    expr = re.compile(r'/\s', re.MULTILINE | re.UNICODE);
+
+    f = open(input_filename, 'r')
+    for line in f:
+        line = line.decode('utf-8')
+        line = expr.sub(u'<br/>\n', line)
+        doc_file.write(line.encode('utf-8'))
+    f.close()
+
+    doc_file.seek(0);
+
+    parser = etree.XMLParser(remove_blank_text=True)
+    doc = etree.parse(doc_file, parser)
+
+    result = doc.xslt(style)
+    result.write(output_filename, xml_declaration=True, pretty_print=True, encoding='utf-8')
 
 
 class Fragment(object):
@@ -13,10 +62,10 @@ class Fragment(object):
         self.id = id
         self.themes = themes
         self.events = []
-        
+
     def append(self, event, element):
         self.events.append((event, element))
-    
+
     def closed_events(self):
         stack = []
         for event, element in self.events:
@@ -27,10 +76,10 @@ class Fragment(object):
                     stack.pop()
                 except IndexError:
                     print 'CLOSED NON-OPEN TAG:', element
-        
+
         stack.reverse()
         return self.events + stack
-    
+
     def to_string(self):
         result = []
         for event, element in self.closed_events():
@@ -44,9 +93,9 @@ class Fragment(object):
                     result.append(element.tail)
             else:
                 result.append(element)
-        
+
         return ''.join(result)
-    
+
     def __unicode__(self):
         return self.to_string()
 
@@ -55,28 +104,28 @@ def extract_fragments(input_filename):
     """Extracts theme fragments from input_filename."""
     open_fragments = {}
     closed_fragments = {}
-    
+
     for event, element in etree.iterparse(input_filename, events=('start', 'end')):
         # Process begin and end elements
         if element.tag == 'span' and element.get('class', '') in ('theme-begin', 'theme-end'):
             if not event == 'end': continue # Process elements only once, on end event
-            
+
             # Open new fragment
             if element.get('class', '') == 'theme-begin':
                 fragment = Fragment(id=element.get('fid'), themes=element.text)
-                
+
                 # Append parents
                 if element.getparent().tag != 'body':
                     parents = [element.getparent()]
                     while parents[-1].getparent().tag != 'body':
                         parents.append(parents[-1].getparent())
-                    
+
                     parents.reverse()
                     for parent in parents:
                         fragment.append('start', parent)
-                
+
                 open_fragments[fragment.id] = fragment
-                    
+
             # Close existing fragment
             else:
                 try:
@@ -86,13 +135,13 @@ def extract_fragments(input_filename):
                 else:
                     closed_fragments[fragment.id] = fragment
                     del open_fragments[fragment.id]
-            
+
             # Append element tail to lost_text (we don't want to lose any text)
             if element.tail:
                 for fragment_id in open_fragments:
                     open_fragments[fragment_id].append('text', element.tail)
-        
-        
+
+
         # Process all elements except begin and end
         else:
             # Omit annotation tags
@@ -103,50 +152,6 @@ def extract_fragments(input_filename):
             else:
                 for fragment_id in open_fragments:
                     open_fragments[fragment_id].append(event, copy.copy(element))
-        
+
     return closed_fragments, open_fragments
-
-
-if __name__ == '__main__':
-    # Parse commandline arguments
-    usage = """Usage: %prog [options] SOURCE [SOURCE...]
-    Extract theme fragments from SOURCE."""
-    
-    parser = optparse.OptionParser(usage=usage)
-    
-    parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False,
-        help='print status messages to stdout')
-    
-    options, input_filenames = parser.parse_args()
-    
-    if len(input_filenames) < 1:
-        parser.print_help()
-        exit(1)
-    
-    # Do some real work
-    for input_filename in input_filenames:
-        if options.verbose:
-            print input_filename
-    
-        output_filename = os.path.splitext(input_filename)[0] + '.fragments.html'
-    
-        closed_fragments, open_fragments = extract_fragments(input_filename)
-
-        for fragment_id in open_fragments:
-            print '%s:warning:unclosed fragment #%s' % (input_filename, fragment_id)
-
-        output_file = open(output_filename, 'w')
-        output_file.write("""
-            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-            <html><head>
-                <title>bookfragments output</title>
-                <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
-                <link rel="stylesheet" href="master.css" type="text/css" media="screen" charset="utf-8" />
-            </head>
-            <body>""")
-        for fragment in closed_fragments.values():
-            html = u'<div class="fragment"><h3>[#%s] %s</h3>%s</div>' % (fragment.id, fragment.themes, fragment)
-            output_file.write(html.encode('utf-8'))
-        output_file.write('</body></html>')
-        output_file.close()
 
