f6925ca9c2f8061e4a170577df07407990222a26
Marek Stępniowski <marek@stepniowski.com>
2008-09-06 15:48:57 +0200

    Made MEDIA_URL relative to application URL.

--- settings.py
+++ settings.py
@@ -44,7 +44,7 @@ MEDIA_ROOT = PROJECT_DIR + '/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8000/media/'
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
