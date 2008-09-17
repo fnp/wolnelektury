c1fbd6ea3930ca7f96604fdb721e2f642827536b
Marek Stępniowski <marek@stepniowski.com>
2008-09-17 14:00:04 +0200

    "Gatunki literackie" -> "Gatunki". Changed SearchForm title.

--- apps/catalogue/forms.py
+++ apps/catalogue/forms.py
@@ -14,7 +14,7 @@ class SearchForm(forms.Form):
     def __init__(self, *args, **kwargs):
         tags = kwargs.pop('tags', [])
         super(SearchForm, self).__init__(*args, **kwargs)
-        self.fields['q'].widget.attrs['title'] = u'tytuł, autor, motyw, epoka, rodzaj, gatunek literacki'
+        self.fields['q'].widget.attrs['title'] = u'tytuł, autor, motyw/temat, epoka, rodzaj, gatunek'
         self.fields['tags'].initial = '/'.join(tag.slug for tag in Tag.get_tag_list(tags))
 
 
