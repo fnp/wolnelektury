41dd82e16547d7a34dd7a29a63e8ef921cf86b12
Marek Stępniowski <marek@stepniowski.com>
2008-09-18 22:07:59 +0200

    Added XML with genre families specification and script converting it to HTML.

---
+++ conv_genre_families.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from lxml import etree
+from slughifi import slughifi
+from django.core.management import setup_environ
+from wolnelektury import settings
+
+setup_environ(settings)
+
+from catalogue.models import Tag
+
+
+doc = etree.parse('rodziny.xml')
+
+for element in doc.findall('//span'):
+    themes = [s.strip() for s in element.text.split(',')]
+    
+    element.text = u''
+    
+    for theme in themes:
+        try:
+            Tag.objects.get(slug=slughifi(theme))
+        
+            link = etree.SubElement(element, 'a', href=u'/katalog/%s' % slughifi(theme))
+            link.text = theme
+            link.tail = ', '
+            last_link = link
+        except:
+            print "Pomijam %s" % slughifi(theme)
+
+    last_link.tail = ''
+
+
+doc.write('ok.xml', xml_declaration=False, pretty_print=True, encoding='utf-8')
