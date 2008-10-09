2ef63bb1d6e81ac9b2251f201615a00f0a458f35
Marek Stępniowski <marek@stepniowski.com>
2008-10-09 12:44:26 +0200

    Removed unneeded dispatcher import from catalogue.fields module.

--- apps/catalogue/fields.py
+++ apps/catalogue/fields.py
@@ -4,7 +4,6 @@ import datetime
 from django.conf import settings
 from django.db import models
 from django.db.models import signals
-from django.dispatch import dispatcher
 from django import forms
 from django.forms.widgets import flatatt
 from django.forms.util import smart_unicode
