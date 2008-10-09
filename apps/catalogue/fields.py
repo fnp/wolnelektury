0a428c3de0e6bcfc863f105101ec1bdb44bbf9de
Marek Stępniowski <marek@stepniowski.com>
2008-10-09 11:45:37 +0200

    Added JsonField to catalogue app.

--- apps/catalogue/fields.py
+++ apps/catalogue/fields.py
@@ -1,10 +1,66 @@
 # -*- coding: utf-8 -*-
+import datetime
+
+from django.conf import settings
+from django.db import models
+from django.db.models import signals
+from django.dispatch import dispatcher
 from django import forms
 from django.forms.widgets import flatatt
 from django.forms.util import smart_unicode
+from django.utils import simplejson as json
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
-from django.utils.simplejson import dumps
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, datetime.date):
+            return obj.strftime('%Y-%m-%d')
+        elif isinstance(obj, datetime.time):
+            return obj.strftime('%H:%M:%S')
+        return json.JSONEncoder.default(self, obj)
+
+
+def dumps(data):
+    return JSONEncoder().encode(data)
+
+
+def loads(str):
+    return json.loads(str, encoding=settings.DEFAULT_CHARSET)
+
+
+class JSONField(models.TextField):
+    def db_type(self):
+        return 'text'
+    
+    def get_internal_type(self):
+        return 'TextField'
+
+    def pre_save(self, model_instance, add):
+        value = getattr(model_instance, self.attname, None)
+        return dumps(value)
+
+    def contribute_to_class(self, cls, name):
+        super(JSONField, self).contribute_to_class(cls, name)
+        dispatcher.connect(self.post_init, signal=signals.post_init, sender=cls)
+
+        def get_json(model_instance):
+            return dumps(getattr(model_instance, self.attname, None))
+        setattr(cls, 'get_%s_json' % self.name, get_json)
+
+        def set_json(model_instance, json):
+            return setattr(model_instance, self.attname, loads(json))
+        setattr(cls, 'set_%s_json' % self.name, set_json)
+
+    def post_init(self, instance=None):
+        value = self.value_from_object(instance)
+        if (value):
+            setattr(instance, self.attname, loads(value))
+        else:
+            setattr(instance, self.attname, None)
 
 
 class JQueryAutoCompleteWidget(forms.TextInput):
@@ -32,7 +88,7 @@ class JQueryAutoCompleteWidget(forms.TextInput):
             final_attrs['id'] = 'id_%s' % name
         
         html = u'''<input type="text" %(attrs)s/>
-            <script type="text/javascript"><!--//
+            <script type="text/javascript">//<!--
             %(js)s//--></script>
             ''' % {
                 'attrs' : flatatt(final_attrs),
