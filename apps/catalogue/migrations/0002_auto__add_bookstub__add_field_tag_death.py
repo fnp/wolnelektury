02cc64eab3d1bdc7503b3fd4386a8da8ea59f09d
Łukasz Rekucki <lrekucki@gmail.com>
2010-05-27 20:26:05 +0200

    Regenerated add_book and add_field_tag migrations.

--- apps/catalogue/migrations/0002_auto__add_bookstub__add_field_tag_death.py
+++ apps/catalogue/migrations/0002_auto__add_bookstub__add_field_tag_death.py
@@ -1,48 +1,54 @@
-# -*- coding: utf-8 -*-
-
+# encoding: utf-8
+import datetime
 from south.db import db
+from south.v2 import SchemaMigration
 from django.db import models
-from catalogue.models import *
 
-class Migration:
+class Migration(SchemaMigration):
     
     def forwards(self, orm):
         
         # Adding model 'BookStub'
         db.create_table('catalogue_bookstub', (
-            ('id', orm['catalogue.BookStub:id']),
-            ('title', orm['catalogue.BookStub:title']),
-            ('author', orm['catalogue.BookStub:author']),
-            ('pd', orm['catalogue.BookStub:pd']),
-            ('slug', orm['catalogue.BookStub:slug']),
-            ('translator', orm['catalogue.BookStub:translator']),
-            ('translator_death', orm['catalogue.BookStub:translator_death']),
+            ('author', self.gf('django.db.models.fields.CharField')(max_length=120)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=120)),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('translator', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('translator_death', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('pd', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=120, db_index=True)),
         ))
         db.send_create_signal('catalogue', ['BookStub'])
-        
+
+        # Adding field 'Tag.death'
+        db.add_column('catalogue_tag', 'death', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True), keep_default=False)
     
     
     def backwards(self, orm):
         
         # Deleting model 'BookStub'
         db.delete_table('catalogue_bookstub')
-        
+
+        # Deleting field 'Tag.death'
+        db.delete_column('catalogue_tag', 'death')
     
     
     models = {
         'auth.group': {
+            'Meta': {'object_name': 'Group'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
             'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
         },
         'auth.permission': {
-            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
         'auth.user': {
+            'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
@@ -58,10 +64,11 @@ class Migration:
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
         'catalogue.book': {
+            'Meta': {'object_name': 'Book'},
             '_short_html': ('django.db.models.fields.TextField', [], {}),
             'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'extra_info': ('JSONField', ["_('extra information')"], {}),
+            'extra_info': ('catalogue.fields.JSONField', [], {}),
             'gazeta_link': ('django.db.models.fields.CharField', [], {'max_length': '240', 'blank': 'True'}),
             'html_file': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -78,6 +85,7 @@ class Migration:
             'xml_file': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'blank': 'True'})
         },
         'catalogue.bookstub': {
+            'Meta': {'object_name': 'BookStub'},
             'author': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'pd': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
@@ -87,6 +95,7 @@ class Migration:
             'translator_death': ('django.db.models.fields.TextField', [], {'blank': 'True'})
         },
         'catalogue.fragment': {
+            'Meta': {'object_name': 'Fragment'},
             '_short_html': ('django.db.models.fields.TextField', [], {}),
             'anchor': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
             'book': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fragments'", 'to': "orm['catalogue.Book']"}),
@@ -95,6 +104,7 @@ class Migration:
             'text': ('django.db.models.fields.TextField', [], {})
         },
         'catalogue.tag': {
+            'Meta': {'object_name': 'Tag'},
             'book_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'category': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
             'death': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
@@ -109,14 +119,14 @@ class Migration:
             'wiki_link': ('django.db.models.fields.CharField', [], {'max_length': '240', 'blank': 'True'})
         },
         'catalogue.tagrelation': {
-            'Meta': {'unique_together': "(('tag', 'content_type', 'object_id'),)", 'db_table': "'catalogue_tag_relation'"},
+            'Meta': {'unique_together': "(('tag', 'content_type', 'object_id'),)", 'object_name': 'TagRelation', 'db_table': "'catalogue_tag_relation'"},
             'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'items'", 'to': "orm['catalogue.Tag']"})
         },
         'contenttypes.contenttype': {
-            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
