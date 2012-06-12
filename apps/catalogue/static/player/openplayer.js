c62abdd7f36455aacc7cbd04d944576013019f53
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2012-06-12 13:35:08 +0200

    fixes #2159: scrollbars in audioplayer

--- apps/catalogue/static/player/openplayer.js
+++ apps/catalogue/static/player/openplayer.js
@@ -7,7 +7,7 @@ $('.open-player').click(function(event) {
     event.preventDefault();
     window.open($(this).attr('href'),
         'player',
-        'width=422, height=500'
+        'width=422, height=500, scrollbars=1'
         );
 });
 
