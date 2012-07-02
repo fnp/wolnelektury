0aafa951948f151fb74c704d7901d9bbc136ac74
Marcin Koziej <marcin@lolownia.org>
2012-07-02 13:54:56 +0200

    fix of a fix

--- apps/opds/views.py
+++ apps/opds/views.py
@@ -390,7 +390,8 @@ class SearchFeed(AcquisitionFeed):
 
             if author:
                 log.info( "narrow to author %s" % author)
-                hint.tags(srch.search_tags(srch.get_tokens(author, field='authors'), filt=srch.term_filter(Term('tag_category', 'author'))))
+                hint.tags(srch.search_tags(srch.make_phrase(srch.get_tokens(author, field='authors'), field='authors'), 
+                                            filt=srch.term_filter(Term('tag_category', 'author'))))
 
             if translator:
                 log.info( "filter by translator %s" % translator)
