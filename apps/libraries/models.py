a239e92f83a56b9f9951594bcb4774c0f8f4d500
Aleksander Łukasz <aleksander.lukasz@nowoczesnapolska.org.pl>
2013-05-08 16:38:47 +0200

    cleanup

--- apps/libraries/models.py
+++ apps/libraries/models.py
@@ -4,10 +4,9 @@ from django.utils.translation import ugettext_lazy as _
 class Library(models.Model):
     """Represent a single library in the libraries dictionary"""
 
-    #slug = models.SlugField(_('slug'), max_length=120, unique=True, db_index=True)
-    name = models.CharField(_('name'), max_length=120, blank=True)
-    url = models.CharField(_('url'), max_length=120, blank=True)
-    description = models.TextField(_('description'), blank=True)
+    name = models.CharField(_('name'), max_length = 120, blank = True)
+    url = models.CharField(_('url'), max_length = 120, blank = True)
+    description = models.TextField(_('description'), blank = True)
 
     class Meta:
         verbose_name = _('library')
