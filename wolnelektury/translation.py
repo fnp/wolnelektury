4c0a2cdc4ea9531a47943ececfa3dc656a0462f1
Radek Czajka <radekczajka@nowoczesnapolska.org.pl>
2013-11-21 15:53:34 +0100

    i18n for collection, tag wiki_links

--- wolnelektury/translation.py
+++ wolnelektury/translation.py
@@ -4,15 +4,19 @@
 #
 
 from modeltranslation.translator import translator, TranslationOptions
-from catalogue.models import Tag
+from catalogue.models import Collection, Tag
 from infopages.models import InfoPage
 
 class InfoPageTranslationOptions(TranslationOptions):
     fields = ('title', 'left_column', 'right_column')
 
 class TagTranslationOptions(TranslationOptions):
-    fields = ('name', 'description')
+    fields = ('name', 'description', 'wiki_link')
+
+class CollectionTranslationOptions(TranslationOptions):
+    fields = ('title', 'description')
 
 translator.register(InfoPage, InfoPageTranslationOptions)
 translator.register(Tag, TagTranslationOptions)
+translator.register(Collection, CollectionTranslationOptions)
 
