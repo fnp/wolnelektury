cf60318446030eca4a2f3ec435ba67fc03161cfb
Jan Szejko <jan.szejko@gmail.com>
2016-05-24 16:21:50 +0200

    translation update

--- src/wolnelektury/__init__.py
+++ src/wolnelektury/__init__.py
@@ -2,6 +2,11 @@
 # This file is part of Wolnelektury, licensed under GNU Affero GPLv3 or later.
 # Copyright © Fundacja Nowoczesna Polska. See NOTICE for more information.
 #
+from django.utils.translation import gettext
+
 from .celery import app as celery_app
 
 default_app_config = 'wolnelektury.apps.WLCoreConfig'
+
+if False:
+    gettext("Please enter a correct %(username)s and password. Note that both fields may be case-sensitive.")
