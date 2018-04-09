04ed822e3985e1565a1d652017be4004b4ddf8f3
Jan Szejko <janek37@gmail.com>
2018-04-09 16:20:53 +0200

    add option to enable openid (disabled by default)

--- src/wolnelektury/context_processors.py
+++ src/wolnelektury/context_processors.py
@@ -8,5 +8,6 @@ from django.conf import settings
 def extra_settings(request):
     return {
         'STATIC_URL': settings.STATIC_URL,
-        'FULL_STATIC_URL': request.build_absolute_uri(settings.STATIC_URL)
+        'FULL_STATIC_URL': request.build_absolute_uri(settings.STATIC_URL),
+        'USE_OPENID': getattr(settings, 'USE_OPENID', False)
     }
