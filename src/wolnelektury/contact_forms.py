92d5e49e4342aaa19efdc77e66d59e7c16c501bb
Jan Szejko <janek37@gmail.com>
2018-08-17 15:59:41 +0200

    form for #cojaczytam

--- src/wolnelektury/contact_forms.py
+++ src/wolnelektury/contact_forms.py
@@ -70,6 +70,102 @@ class KonkursForm(ContactForm):
               u'rozpowszechnianie ich wizerunków.')
 
 
+class CoJaCzytamForm(ContactForm):
+    form_tag = 'cojaczytam'
+    form_title = u"#cojaczytam? Co Ty czytasz? A oni? Pokażcie nam, że czytacie!"
+    admin_list = ['opiekun_nazwisko', 'contact', 'nazwa_kampanii']
+    ends_on = (2018, 11, 8)
+    disabled_template = 'contact/disabled_contact_form.html'
+    submit_label = u'Wyślij'
+
+    opiekun_nazwisko = forms.CharField(label=u'Imię i nazwisko Opiekuna/ki', max_length=128)
+    contact = forms.EmailField(label=u'Adres e-mail Opiekuna/ki', max_length=128)
+    opiekun_tel = forms.CharField(label=u'Numer telefonu Opiekuna/ki', max_length=32)
+    nazwa_dkk = forms.CharField(label=u'Nazwa szkoły/biblioteki publicznej', max_length=128)
+    adres_dkk = forms.CharField(label=u'Adres szkoły/biblioteki publicznej', max_length=128)
+
+    nazwa_kampanii = forms.CharField(label=u'Nazwa kampanii', max_length=255)
+
+    wiek = forms.ChoiceField(label=u'Grupa wiekowa', choices=(
+        ('9-14', u'uczniowie kl. IV-VIII szkół podstawowych w wieku 9-14 lat,'),
+        ('15-19', u'uczniowie gimnazjum oraz wszystkich typów szkół ponadpodstawowych w wieku 15-19.'),
+    ), widget=forms.RadioSelect)
+
+    uczestnik1_header = HeaderField(label=u'Dane\xa0Uczestników (3 do 5)')
+    uczestnik1_imie = forms.CharField(label=u'Imię', max_length=128)
+    uczestnik1_nazwisko = forms.CharField(label=u'Nazwisko', max_length=128)
+    uczestnik1_email = forms.EmailField(label=u'Adres e-mail', max_length=128)
+    uczestnik2_header = HeaderField(label=u'')
+    uczestnik2_imie = forms.CharField(label=u'Imię', max_length=128)
+    uczestnik2_nazwisko = forms.CharField(label=u'Nazwisko', max_length=128)
+    uczestnik2_email = forms.EmailField(label=u'Adres e-mail', max_length=128)
+    uczestnik3_header = HeaderField(label=u'')
+    uczestnik3_imie = forms.CharField(label=u'Imię', max_length=128)
+    uczestnik3_nazwisko = forms.CharField(label=u'Nazwisko', max_length=128)
+    uczestnik3_email = forms.EmailField(label=u'Adres e-mail', max_length=128)
+    uczestnik4_header = HeaderField(label=u'')
+    uczestnik4_imie = forms.CharField(label=u'Imię', max_length=128, required=False)
+    uczestnik4_nazwisko = forms.CharField(label=u'Nazwisko', max_length=128, required=False)
+    uczestnik4_email = forms.EmailField(label=u'Adres e-mail', max_length=128, required=False)
+    uczestnik5_header = HeaderField(label=u'')
+    uczestnik5_imie = forms.CharField(label=u'Imię', max_length=128, required=False)
+    uczestnik5_nazwisko = forms.CharField(label=u'Nazwisko', max_length=128, required=False)
+    uczestnik5_email = forms.EmailField(label=u'Adres e-mail', max_length=128, required=False)
+
+    ankieta_header = HeaderField(label=u'')
+    opis_kampanii = forms.CharField(
+        label=u'Krótki opis realizacji oraz przebiegu kampanii', max_length=255, widget=forms.Textarea)
+    co_sie_udalo = forms.CharField(label=u'Co udało Wam się zrealizować?', max_length=1024, widget=forms.Textarea)
+    co_sie_nie_udalo = forms.CharField(
+        label=u'Czy jest coś, co chcieliście zrealizować, a się nie udało? Jeśli tak, to dlaczego?', max_length=1024,
+        widget=forms.Textarea)
+    wnioski = forms.CharField(
+        label=u'Jakie wnioski na przyszłość wyciągnęliście z tego, co się nie udało zrealizować?', max_length=1024,
+        widget=forms.Textarea)
+    zasieg = forms.CharField(
+        label=u'Do ilu odbiorców udało Wam się dotrzeć z Waszą kompanią? Podaj liczbę, może być szacunkowa.',
+        max_length=1024, widget=forms.Textarea)
+    grupy_odbiorcow = forms.CharField(
+        label=u'Do jakich grup odbiorców dotarliście (np. uczniowie, nauczyciele, rodzice, seniorzy, inni)?',
+        max_length=1024, widget=forms.Textarea)
+    plik = forms.FileField(
+        label=u'Plik .zip ze stworzonymi materiałami (np. zdjęcia, dokumenty tekstowe)')
+    materialy = forms.CharField(
+        label=u'Adresy stworzonych materiałów online (jeśli dotyczy)', max_length=1024, widget=forms.Textarea,
+        required=False)
+
+    agree_header = HeaderField(label=u'Oświadczenia')
+    agree_terms = forms.BooleanField(
+        label='Regulamin',
+        help_text=mark_safe_lazy(
+            u'Znam i akceptuję <a href="/media/chunks/attachment/Regulamin_konkursu_cojaczytam_edycja_2018.pdf">'
+            u'Regulamin Konkursu</a>.'),
+    )
+    agree_data = forms.BooleanField(
+        label='Przetwarzanie danych osobowych',
+        help_text=u'Oświadczam, że wyrażam zgodę na przetwarzanie danych osobowych zawartych w niniejszym formularzu '
+              u'zgłoszeniowym przez Fundację Nowoczesna Polska (administratora danych) z siedzibą w Warszawie (00-514) '
+              u'przy ul. Marszałkowskiej 84/92 lok. 125 na potrzeby organizacji Konkursu. Jednocześnie oświadczam, '
+              u'że zostałam/em poinformowana/y o tym, że mam prawo wglądu w treść swoich danych i możliwość ich '
+              u'poprawiania oraz że ich podanie jest dobrowolne, ale niezbędne do dokonania zgłoszenia.')
+    agree_license = forms.BooleanField(
+        label='Licencja',
+        help_text=mark_safe_lazy(
+            u'Wyrażam zgodę oraz potwierdzam, że uczestnicy (lub ich przedstawiciele ustawowi – gdy dotyczy) '
+            u'wyrazili zgodę na korzystanie z opowiadania zgodnie z postanowieniami '
+            u'<a href="http://freedomdefined.org/Definition/Pl">wolnej licencji</a>, takiej jak '
+            u'<a href="https://creativecommons.org/licenses/by-sa/3.0/pl/">Creative Commons Uznanie autorstwa – '
+            u'Na tych samych warunkach 3.0 PL</a>. Licencja pozwala każdemu na swobodne, nieodpłatne korzystanie '
+            u'z utworu '
+            u'w oryginale oraz w postaci opracowań do wszelkich celów wymagając poszanowania autorstwa i innych praw '
+            u'osobistych oraz tego, aby ewentualne opracowania utworu były także udostępniane na tej samej licencji.'))
+    agree_wizerunek = forms.BooleanField(
+        label='Rozpowszechnianie wizerunku',
+        help_text=u'Wyrażam zgodę oraz potwierdzam, że uczestnicy (lub ich przedstawiciele ustawowi – gdy dotyczy) '
+                  u'wyrazili zgodę na nagrywanie i następnie rozpowszechnianie ich '
+                  u'wizerunków w celach promocyjnych.')
+
+
 class WorkshopsForm(ContactForm):
     form_tag = 'warsztaty'
     form_title = u"Wolne Lektury Fest"
