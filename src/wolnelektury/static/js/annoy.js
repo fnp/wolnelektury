746bfb66224e6a1065e5355ed63f9c4836b0a498
Radek Czajka <rczajka@rczajka.pl>
2019-02-08 11:27:01 +0100

    Summon the annoying banner.

--- src/wolnelektury/static/js/annoy.js
+++ src/wolnelektury/static/js/annoy.js
@@ -6,18 +6,18 @@ $("#annoy-on").click(function(e) {
     e.preventDefault();
     $("#annoy").slideDown('fast');
     $(this).hide();
-    if (Modernizr.localstorage) localStorage.removeItem("annoyed2013");
+    if (Modernizr.localstorage) localStorage.removeItem("annoyed2019");
 });
 
 $("#annoy-off").click(function() {
     $("#annoy").slideUp('fast');
     $("#annoy-on").show();
-    if (Modernizr.localstorage) localStorage["annoyed2013"] = true;
+    if (Modernizr.localstorage) localStorage["annoyed2019"] = true;
 });
 
 
 if (Modernizr.localstorage) {
-    if (!localStorage["annoyed2013"]) {
+    if (!localStorage["annoyed2019"]) {
         $("#annoy-on").hide();
         $("#annoy").show();
     }
