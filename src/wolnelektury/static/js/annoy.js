ad5acb886787871a70eee6385cddbb4238d3adb2
Radek Czajka <rczajka@rczajka.pl>
2019-06-17 15:30:47 +0200

    New annoying banner.

--- src/wolnelektury/static/js/annoy.js
+++ src/wolnelektury/static/js/annoy.js
@@ -1,23 +1,22 @@
 (function($) {
     $(function() {
 
-
 $("#annoy-on").click(function(e) {
     e.preventDefault();
     $("#annoy").slideDown('fast');
     $(this).hide();
-    if (Modernizr.localstorage) localStorage.removeItem("annoyed2019");
+    if (Modernizr.localstorage) localStorage.removeItem("annoyed2019tpwl");
 });
 
 $("#annoy-off").click(function() {
     $("#annoy").slideUp('fast');
     $("#annoy-on").show();
-    if (Modernizr.localstorage) localStorage["annoyed2019"] = true;
+    if (Modernizr.localstorage) localStorage["annoyed2019tpwl"] = true;
 });
 
 
 if (Modernizr.localstorage) {
-    if (!localStorage["annoyed2019"]) {
+    if (!localStorage["annoyed2019tpwl"]) {
         $("#annoy-on").hide();
         $("#annoy").show();
     }
