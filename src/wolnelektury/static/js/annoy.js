3b76a7f9bd1f9dd2e099e76aadc189222beba8f2
Radek Czajka <rczajka@rczajka.pl>
2019-11-13 08:54:40 +0100

    Annoy

--- src/wolnelektury/static/js/annoy.js
+++ src/wolnelektury/static/js/annoy.js
@@ -1,22 +1,24 @@
 (function($) {
     $(function() {
 
+var tag = "annoyed2019wspieraj";
+
 $("#annoy-on").click(function(e) {
     e.preventDefault();
     $("#annoy").slideDown('fast');
     $(this).hide();
-    if (Modernizr.localstorage) localStorage.removeItem("annoyed2019tpwl");
+    if (Modernizr.localstorage) localStorage.removeItem(tag);
 });
 
 $("#annoy-off").click(function() {
     $("#annoy").slideUp('fast');
     $("#annoy-on").show();
-    if (Modernizr.localstorage) localStorage["annoyed2019tpwl"] = true;
+    if (Modernizr.localstorage) localStorage[tag] = true;
 });
 
 
 if (Modernizr.localstorage) {
-    if (!localStorage["annoyed2019tpwl"]) {
+    if (!localStorage[tag]) {
         $("#annoy-on").hide();
         $("#annoy").show();
     }
