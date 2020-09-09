04bcff7bdb003dcd9740926bfc1243859b9defe4
Radek Czajka <rczajka@rczajka.pl>
2020-09-09 22:18:45 +0200

    Wording changes.

--- src/wolnelektury/__init__.py
+++ src/wolnelektury/__init__.py
@@ -6,6 +6,3 @@ from django.utils.translation import gettext
 from .celery import app as celery_app
 
 default_app_config = 'wolnelektury.apps.WLCoreConfig'
-
-if False:
-    gettext("Please enter a correct %(username) and password. Note that both fields may be case-sensitive.")
