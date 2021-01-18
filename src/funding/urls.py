56a76b9dc0745d22d9b1eac54348d6109dc3c0e1
Radek Czajka <rczajka@rczajka.pl>
2021-01-18 14:07:22 +0100

    Disable banners on funding pages.

--- src/funding/urls.py
+++ src/funding/urls.py
@@ -2,21 +2,22 @@
 # Copyright © Fundacja Nowoczesna Polska. See NOTICE for more information.
 #
 from django.urls import path, include
+from annoy.utils import banner_exempt
 from . import views
 
 
 urlpatterns = [
-    path('', views.CurrentView.as_view(), name='funding_current'),
-    path('teraz/', views.CurrentView.as_view()),
-    path('teraz/<slug:slug>/', views.CurrentView.as_view(), name='funding_current'),
-    path('lektura/', views.OfferListView.as_view(), name='funding'),
-    path('lektura/<slug:slug>/', views.OfferDetailView.as_view(), name='funding_offer'),
-    path('pozostale/', views.WLFundView.as_view(), name='funding_wlfund'),
+    path('', banner_exempt(views.CurrentView.as_view()), name='funding_current'),
+    path('teraz/', banner_exempt(views.CurrentView.as_view())),
+    path('teraz/<slug:slug>/', banner_exempt(views.CurrentView.as_view()), name='funding_current'),
+    path('lektura/', banner_exempt(views.OfferListView.as_view()), name='funding'),
+    path('lektura/<slug:slug>/', banner_exempt(views.OfferDetailView.as_view()), name='funding_offer'),
+    path('pozostale/', banner_exempt(views.WLFundView.as_view()), name='funding_wlfund'),
 
-    path('dziekujemy/', views.ThanksView.as_view(), name='funding_thanks'),
-    path('niepowodzenie/', views.NoThanksView.as_view(), name='funding_nothanks'),
+    path('dziekujemy/', banner_exempt(views.ThanksView.as_view()), name='funding_thanks'),
+    path('niepowodzenie/', banner_exempt(views.NoThanksView.as_view()), name='funding_nothanks'),
 
-    path('wylacz_email/', views.DisableNotifications.as_view(), name='funding_disable_notifications'),
+    path('wylacz_email/', banner_exempt(views.DisableNotifications.as_view()), name='funding_disable_notifications'),
 
     path('getpaid/', include('getpaid.urls')),
 ]
