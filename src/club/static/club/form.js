5d9b7d5f8dc95660c8d52da7329d3efd87947410
Radek Czajka <rczajka@rczajka.pl>
2021-04-27 11:21:52 +0200

    Add campaign custom dimension to donation form.

--- src/club/static/club/form.js
+++ src/club/static/club/form.js
@@ -18,7 +18,18 @@ $(function() {
     $("#payment-form .plan").change(update_methods);
 
 
+    function qs(key) {
+        key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&"); // escape RegEx meta chars
+        var match = location.search.match(new RegExp("[?&]"+key+"=([^&]+)(&|$)"));
+        return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+    }
+
     $("#payment-form").submit(function() {
-        _paq.push(['trackGoal', 12, parseFloat($("#id_amount").val())]);
+        let camp = qs('pk_campaign');
+        if (!camp && window.location.pathname !== "/towarzystwo/") {
+            camp = window.location.pathname;
+        }
+        let dims = camp ? {dimension2: camp} : {};
+        _paq.push(['trackGoal', 12, parseFloat($("#id_amount").val()), dims]);
     });
 });
