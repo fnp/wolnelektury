ff21f9bc563f16fcde81eafccd38f212f22ba582
Radek Czajka <rczajka@rczajka.pl>
2021-05-20 09:24:33 +0200

    Basic eink version.

--- src/wolnelektury/context_processors.py
+++ src/wolnelektury/context_processors.py
@@ -8,5 +8,6 @@ def extra_settings(request):
     return {
         'STATIC_URL': settings.STATIC_URL,
         'FULL_STATIC_URL': request.build_absolute_uri(settings.STATIC_URL),
-        'USE_OPENID': getattr(settings, 'USE_OPENID', False)
+        'USE_OPENID': getattr(settings, 'USE_OPENID', False),
+        'VARIANT': settings.VARIANTS.get(request.get_host()),
     }
