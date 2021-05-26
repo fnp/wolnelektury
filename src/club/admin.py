cbe7ef2f04d751e6f719299632d77cfcb29be8f1
Radek Czajka <rczajka@rczajka.pl>
2021-05-26 14:36:59 +0200

    Filter schedules by source.

--- src/club/admin.py
+++ src/club/admin.py
@@ -57,7 +57,7 @@ class ExpiredFilter(YesNoFilter):
 class ScheduleAdmin(admin.ModelAdmin):
     list_display = ['email', 'started_at', 'payed_at', 'expires_at', 'amount', 'monthly', 'yearly', 'is_cancelled']
     search_fields = ['email']
-    list_filter = ['is_cancelled', 'monthly', 'yearly', PayedFilter, ExpiredFilter]
+    list_filter = ['is_cancelled', 'monthly', 'yearly', PayedFilter, ExpiredFilter, 'source']
     date_hierarchy = 'started_at'
     raw_id_fields = ['membership']
     inlines = [PayUOrderInline, PayUCardTokenInline]
