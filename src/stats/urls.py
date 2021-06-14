d32d20846a6b20dc6059dfde22515071e48f9ec0
Radek Czajka <rczajka@rczajka.pl>
2021-06-14 11:51:19 +0200

    Daily stats view.

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('top/', views.TopView.as_view(), name='stats_top'),
-    path('top/daily/', views.TopView.as_view(), name='stats_top'),
+    path('top/daily/', views.DailyTopView.as_view(), name='stats_top'),
 ]
