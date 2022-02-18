15cae7d86e624d8afdb7eac794b58990ded9e64b
Radek Czajka <rczajka@rczajka.pl>
2022-02-18 10:46:57 +0100

    fix

--- src/paypal/rest.py
+++ src/paypal/rest.py
@@ -1,7 +1,7 @@
 # This file is part of Wolnelektury, licensed under GNU Affero GPLv3 or later.
 # Copyright © Fundacja Nowoczesna Polska. See NOTICE for more information.
 #
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from decimal import Decimal
 import paypalrestsdk
 import pytz
