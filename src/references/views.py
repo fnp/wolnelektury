a94e848fd865f0a887850e4858dab64c426b10db
Radek Czajka <rczajka@rczajka.pl>
2023-06-10 19:02:37 +0200

    experimental map

--- src/references/views.py
+++ src/references/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+def map(request):
+    return render(request, 'references/map.html', {
+        'entities': models.Entity.objects.exclude(lat=None).exclude(lon=None),
+    })
+                  
