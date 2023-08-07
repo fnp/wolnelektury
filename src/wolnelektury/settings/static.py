759e23384cc103d4d313d9a5486644d59142fe69
Radek Czajka <rczajka@rczajka.pl>
2023-08-07 13:20:53 +0200

    cleanup

--- src/wolnelektury/settings/static.py
+++ src/wolnelektury/settings/static.py
@@ -32,34 +32,6 @@ PIPELINE = {
             ],
             'output_filename': 'css/compressed/2022.css',
         },
-        'main': {
-            # styles both for mobile and for big screen
-            'source_filenames': [
-                'css/import/gelasio.css',
-
-                'css/jquery.countdown.css',
-                'jplayer/jplayer.blue.monday.css',
-
-                'sponsors/css/sponsors.css',
-
-                'annoy/banner.scss',
-
-                'css/ui-lightness/jquery-ui-1.8.16.custom.css',
-
-                'css/tlite.css',
-
-                'scss/main.scss',
-                'club/club.scss',
-            ],
-            'output_filename': 'css/compressed/main.css',
-        },
-        'book': {
-            'source_filenames': [
-                'css/import/gelasio.css',
-                'css/master.book.css',
-            ],
-            'output_filename': 'css/compressed/book.css',
-        },
         'book_text': {
             'source_filenames': [
                 'css/import/gelasio.css',
@@ -73,71 +45,12 @@ PIPELINE = {
             ],
             'output_filename': 'css/compressed/book_text.css',
         },
-        'forum': {
-            'source_filenames': [
-                'scss/forum.scss',
-            ],
-            'output_filename': 'css/compressed/compressed/forum.css',
-        },
-        'picture': {
-            'source_filenames': [
-                'css/import/gelasio.css',
-                'css/master.book.css',
-                'css/master.picture.css',
-            ],
-            'output_filename': 'css/compressed/picture.css',
-        },
-        'player': {
-            'source_filenames': [
-                'jplayer/jplayer.blue.monday.css',
-                'player/player.css',
-            ],
-            'output_filename': 'css/compressed/player.css',
-        },
-        'simple': {
-            'source_filenames': ('css/simple.css',),
-            'output_filename': 'css/compressed/simple.css',
-        },
         'widget': {
             'source_filenames': ('scss/widget.scss',),
             'output_filename': 'css/compressed/widget.css',
         },
     },
     'JAVASCRIPT': {
-        'base': {
-            'source_filenames': (
-                'js/contrib/jquery.cycle2.min.js',
-                'js/contrib/jquery.jqmodal.js',
-                'js/contrib/jquery.form.js',
-                'js/contrib/jquery.paging.min.js',
-                'js/contrib/jquery.countdown.js', 'js/contrib/jquery.countdown-pl.js',
-                'js/contrib/jquery.countdown-de.js', 'js/contrib/jquery.countdown-uk.js',
-                'js/contrib/jquery.countdown-es.js', 'js/contrib/jquery.countdown-lt.js',
-                'js/contrib/jquery.countdown-ru.js', 'js/contrib/jquery.countdown-fr.js',
-                'js/contrib/jquery.shorten.js',
-
-                'js/contrib/jquery-ui-1.8.16.custom.min.js',
-
-                'js/contrib/tlite.min.js',
-
-                'jplayer/jquery.jplayer.min.js',
-                'jplayer/jplayer.playlist.min.js',
-                'player/player.js',
-
-                'js/locale.js',
-                'js/dialogs.js',
-                'js/base.js',
-                'pdcounter/pdcounter.js',
-                'sponsors/js/sponsors.js',
-                'player/openplayer.js',
-                'js/search.js',
-                'funding/funding.js',
-                'club/form.js',
-
-                'annoy/banner.js',
-                ),
-            'output_filename': 'js/base.min.js',
-        },
         '2022': {
             'source_filenames': [
                 '2022/scripts/vendor.js',
@@ -162,14 +75,6 @@ PIPELINE = {
             ],
             'output_filename': 'js/2022.min.js'
         },
-        'player': {
-            'source_filenames': [
-                'jplayer/jquery.jplayer.min.js',
-                'jplayer/jplayer.playlist.min.js',
-                'player/player.js',
-            ],
-            'output_filename': 'js/player.min.js',
-        },
         '2022_player': {
             'source_filenames': [
                 'js/contrib/jplayer/jquery.jplayer.min.js',
@@ -178,23 +83,8 @@ PIPELINE = {
             ],
             'output_filename': 'js/2022_player.min.js',
         },
-        'book': {
-            'source_filenames': [
-                'js/contrib/jquery.eventdelegation.js',
-                'js/contrib/jquery.scrollto.js',
-                'js/contrib/jquery.highlightfade.js',
-                'js/book_text/other.js',
-                'js/book.js',
-
-                'js/contrib/raphael-min.js',
-                'js/contrib/progressSpin.min.js',
-                'js/picture.js',
-            ],
-            'output_filename': 'js/book.min.js',
-        },
         'book_text': {
             'source_filenames': [
-                'js/contrib/jquery.cycle2.min.js',
                 'js/contrib/jquery.form.js',
                 'js/contrib/jquery.jqmodal.js',
                 'js/book_text/info.js',
@@ -209,21 +99,10 @@ PIPELINE = {
 
                 'js/contrib/jquery.highlightfade.js',
                 'js/contrib/raphael-min.js',
-                'player/openplayer.js',
                 'js/contrib/progressSpin.min.js',
                 'annoy/banner.js',
             ],
             'output_filename': 'js/book_text.js',
-        },
-        'picture': {
-            'source_filenames': [
-                'js/picture.js',
-            ],
-            'output_filename': 'js/picture.min.js',
-        },
-        'book_ie': {
-            'source_filenames': ('js/contrib/ierange-m2.js',),
-            'output_filename': 'js/book_ie.min.js',
         },
         'widget': {
             'source_filenames': (
