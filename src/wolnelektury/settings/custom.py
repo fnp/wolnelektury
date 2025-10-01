d19997df630599ea19abc7731341c0608b7eaae1
Radek Czajka <rczajka@rczajka.pl>
2025-10-01 16:10:36 +0200

    Towards easier deployment.

--- src/wolnelektury/settings/custom.py
+++ src/wolnelektury/settings/custom.py
@@ -22,7 +22,8 @@ LIBRARIAN_PDF_MOREFLOATS = None
 
 LATEST_BLOG_POSTS = "https://fundacja.wolnelektury.pl/feed/?cat=-135"
 
-CATALOGUE_COUNTERS_FILE = os.path.join(VAR_DIR, 'catalogue_counters.p')
+CATALOGUE_COUNTERS_FILE = os.path.join(VAR_DIR, 'counters/catalogue_counters.p')
+LESMIANATOR_PICKLE = os.path.join(VAR_DIR, 'counters/lesmianator.p')
 
 CATALOGUE_MIN_INITIALS = 60
 
