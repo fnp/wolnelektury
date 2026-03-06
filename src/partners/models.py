0bcc7528161ce32c08247cf87c6096b888cfb153
Radek Czajka <rczajka@rczajka.pl>
2026-03-06 15:05:26 +0100

    decimal prices

--- src/partners/models.py
+++ src/partners/models.py
@@ -20,7 +20,7 @@ class Partner(models.Model):
 class PriceLevel(models.Model):
     partner = models.ForeignKey(Partner, models.CASCADE)
     min_pages = models.IntegerField(null=True, blank=True)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         ordering = ('price',)
