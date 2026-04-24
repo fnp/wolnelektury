439fc561d2a4929fc82b58bc706a49cc95826eb6
Radek Czajka <rczajka@rczajka.pl>
2026-04-24 11:48:19 +0200

    Protect dictionary from bots.

--- src/dictionary/urls.py
+++ src/dictionary/urls.py
@@ -1,9 +1,10 @@
 # This file is part of Wolne Lektury, licensed under GNU Affero GPLv3 or later.
 # Copyright © Fundacja Wolne Lektury. See NOTICE for more information.
 #
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from dictionary.views import NotesView
 
 urlpatterns = [
-    path('', NotesView.as_view(), name='dictionary_notes'),
+    path('', login_required(NotesView.as_view()), name='dictionary_notes'),
 ]
