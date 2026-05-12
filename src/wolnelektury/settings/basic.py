0636415d54e414aa9ddff61bed92f72f9ee03a21
Radek Czajka <rczajka@rczajka.pl>
2026-05-12 11:49:45 +0200

    Set password validators

--- src/wolnelektury/settings/basic.py
+++ src/wolnelektury/settings/basic.py
@@ -15,6 +15,24 @@ CONTACT_EMAIL = 'fundacja@wolnelektury.pl'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split()
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 6,
+        },
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
 CACHE_MIDDLEWARE_SECONDS = 3 * 60
 
 CACHES = {
